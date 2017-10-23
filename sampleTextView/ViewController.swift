26a2d8b287409c4da6d0834751206c5c5a039133
tatsuki197 <tatsuki197@gmail.com>
2017-10-23 10:10:33 +0800

    Init commit

---
+++ sampleTextView/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  sampleTextView
+//
+//  Created by Tatsuki Nakatsuka on 2017/10/20.
+//  Copyright © 2017年 Tatsuki Nakatsuka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myTextView: UITextView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+         //文字色を変更
+        myTextView.textColor = UIColor.blue
+        
+        //TODO:プロパテ指定を使用してプログラムで以下の変更を行ってください
+        //フォントをAmericanTypewriterに変更
+        myTextView.font = UIFont(name: "AmericanTypewriter", size: 20)
+        
+        //TextViewを中央寄せ
+        
+        
+        //文字の量に合わせて変化していくように設定
+        myTextView.sizeToFit()
+        
+        //文字寄せを中央寄せに変更
+        myTextView.textAlignment = NSTextAlignment.center
+        
+    }
+
+    @IBAction func tapClose(_ sender: UIButton) {
+   
+        
+        //プログラム指定でキーボードを閉じる
+          myTextView.resignFirstResponder()
+    
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
